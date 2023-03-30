9ce8d310f075ecbb8672e52a151acd202a282fc7
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-03-30 06:33:21 +0000

    s3 large file content reading

--- s3/s3-large-file-read/src/index.ts
+++ s3/s3-large-file-read/src/index.ts
@@ -1,3 +1,20 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+const client = new S3Client({});
+
 export const handler = async(event: any) => {
     console.log("Hello, World");
+
+    const getObjectCommand = new GetObjectCommand({
+        Bucket: "",
+        Key: ""
+    });
+
+    try{
+        const response = await client.send(getObjectCommand);
+        const str = await response.Body?.transformToString();
+        console.log("Bucket Content ", str);
+    } catch(error){
+        console.error("Error in getObject ", error);
+    }
 }
