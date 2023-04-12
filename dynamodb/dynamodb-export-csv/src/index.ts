8ec6deab14a7ef0994d2f37f02886602a3b84d0b
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-04-12 09:52:18 +0000

    DynamoDB Query and Export CSV

--- dynamodb/dynamodb-export-csv/src/index.ts
+++ dynamodb/dynamodb-export-csv/src/index.ts
@@ -1,23 +1,27 @@
-import {QueryCommand, DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import { QueryCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {unmarshall} from '@aws-sdk/util-dynamodb';
 
-export const handler = async() => {
+export const handler = async () => {
     console.log("Hello, World");
 
-    try{
-        const ddbClient = new DynamoDBClient({region: "US-WEST-2"});
+    try {
+        const ddbClient = new DynamoDBClient({ region: process.env.REGION });
 
         const queryCommand = new QueryCommand({
             TableName: process.env.TABLE_NAME,
-            IndexName: process.env.INDEX_NAME,
-            KeyConditionExpression: process.env.KEY_CONDITION
+            // IndexName: process.env.INDEX_NAME,
+            KeyConditionExpression: process.env.KEY_CONDITION,
+            ExpressionAttributeValues: {
+                ":param1": { S: "<value>" }
+            }
         });
 
         const result = await ddbClient.send(queryCommand);
         result.Items?.forEach(element => {
-            console.log("element", element);
+            console.log("element", unmarshall(element));
         });
-       
-    } catch(error){
+
+    } catch (error) {
         console.error("Error in DynamoDB Query", error);
     }
 
