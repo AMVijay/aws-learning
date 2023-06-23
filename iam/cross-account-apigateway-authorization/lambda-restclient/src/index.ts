98de53233edfeb02b87aabbdf7eeeba3cbdad828
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-06-23 10:26:22 +0000

    lambda rest client

--- iam/cross-account-apigateway-authorization/lambda-restclient/src/index.ts
+++ iam/cross-account-apigateway-authorization/lambda-restclient/src/index.ts
@@ -2,15 +2,35 @@
  * @author VIJAYaraaghavan Manoharan
  */
 
-import {get} from 'https';
+import { RequestOptions, request } from 'https';
 
-export const handler = async() => {
+export const handler = async () => {
     console.log("Hello, World");
-    console.log("Endpoint value is ", process.env.ENDPOINT );
+    console.log("Endpoint value is ", process.env.ENDPOINT);
 
     const url: string = process.env.ENDPOINT as string;
 
-    get(url, (res) => {
-        console.log("Response from client is ", res);
-    });
+    // Setting the configuration for
+    // the request
+    const options: RequestOptions = {
+        method: 'GET'
+    };
+
+    // Sending the request
+    const req = request(url, options, (res) => {
+        let data = ''
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // Ending the response 
+        res.on('end', () => {
+            console.log('Body:', JSON.parse(data))
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: ", err)
+    }).end()
+
 }
