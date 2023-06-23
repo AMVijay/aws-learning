4b9b7c6c165ae2fe8d606b2c0e7c8df116080b89
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-06-23 09:37:50 +0000

    lambda rest client

--- iam/cross-account-apigateway-authorization/lambda-restclient/src/index.ts
+++ iam/cross-account-apigateway-authorization/lambda-restclient/src/index.ts
@@ -2,6 +2,15 @@
  * @author VIJAYaraaghavan Manoharan
  */
 
+import {get} from 'https';
+
 export const handler = async() => {
     console.log("Hello, World");
+    console.log("Endpoint value is ", process.env.ENDPOINT );
+
+    const url: string = process.env.ENDPOINT as string;
+
+    get(url, (res) => {
+        console.log("Response from client is ", res);
+    });
 }
