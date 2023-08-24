54152b651d51dffc8d7b81c0232501748109e98a
VIJAYaraaghavan Manoharan <AMVijay@users.noreply.github.com>
2023-08-24 13:25:29 +0000

    cloudfront lambda authorizer

--- cloudfront/cloudfront-request-authorization/lambda-fn-cf-authorizer/webpack.config.ts
+++ cloudfront/cloudfront-request-authorization/lambda-fn-cf-authorizer/webpack.config.ts
@@ -3,5 +3,26 @@ import { Configuration } from "webpack";
 const config: Configuration = {
     mode: "production",
     entry: "../src/index.ts",
-    
+    output: {
+        filename: "index.js",
+        library: {
+            type: "commonjs2"
+        }
+    },
+    module: {
+        rules: [
+            // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" }
+        ]
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+        // Add support for TypeScripts fully qualified ESM imports.
+        extensionAlias: {
+            ".js": [".js", ".ts"],
+            ".cjs": [".cjs", ".cts"],
+            ".mjs": [".mjs", ".mts"]
+        }
+    },
 }
