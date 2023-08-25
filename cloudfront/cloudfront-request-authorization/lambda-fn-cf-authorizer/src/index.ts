3662bab2859a9d7c7d68fe8ec107e29d11f01fdf
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-08-25 12:44:58 +0000

    cloudfront lambda authorizer

--- cloudfront/cloudfront-request-authorization/lambda-fn-cf-authorizer/src/index.ts
+++ cloudfront/cloudfront-request-authorization/lambda-fn-cf-authorizer/src/index.ts
@@ -8,18 +8,21 @@ import { Callback, CloudFrontRequest, Context, Handler } from "aws-lambda";
  */
 export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
     console.log("handler method execution");
+    console.log("Event ", event);
     if (checkCloudfrontRequestDetails(event)) {
         const request: CloudFrontRequest = event.Records[0].cf.request;
         if (authorizeCloudfrontRequest(request)) {
             callback(null, request);
         }
         else {
-            callback(null, getUnauthorizedMessage());
+            const response = getUnauthorizedMessage();
+            callback(null, response);
         }
     }
     else {
         // Send Unauthorized Response content
-        callback(null, getUnauthorizedMessage);
+        const response = getUnauthorizedMessage();
+        callback(null, response);
     }
 }
 
