23052c325b5562935661ce6148736286538ab489
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-12-15 08:37:16 +0000

    lambda function to start stop ECS tasks

--- ecs/lambda-start-stop-ecs-service/src/index.ts
+++ ecs/lambda-start-stop-ecs-service/src/index.ts
@@ -1,11 +1,22 @@
 import { Context, Handler } from "aws-lambda";
-import { ECSClient, ListTasksCommand } from '@aws-sdk/client-ecs';
+import { ECSClient, ListTasksCommand, StartTaskCommand, StopTaskCommand } from '@aws-sdk/client-ecs';
 
 export const handler: Handler = async (event: any, context: Context) => {
     console.info("event", JSON.stringify(event));
-    if(event && event.clusterName && event.regionName){
+    if(event && event.clusterName && event.regionName && event.action){
         const tasks = await fetchTasks(event.clusterName, event.regionName);
         console.info("Tasks :: ", tasks);
+        if(event.action === 'stop'){
+            for(const task in tasks){
+                await stopTask(task);
+            }
+        }
+        else if(event.action === 'start'){
+            for(const task in tasks){
+                await startTask(task);
+            } 
+        }
+        
     }
     else{
         return "Invalid Input";
@@ -24,4 +35,23 @@ async function fetchTasks(clusterName: string, regionName: string) {
     const response = await client.send(command);
     console.log("response :: ", response);
     return response.taskArns;
+}
+
+async function stopTask(taskArn: string) {
+    const client = new ECSClient();
+    const command = new StopTaskCommand({
+        task: taskArn
+    });
+    const response = await client.send(command);
+    console.log("stopTask response");
+}
+
+
+async function startTask(taskArn: string) {
+    const client = new ECSClient();
+    // const command = new StartTaskCommand({
+
+    // });
+    // const response = await client.send(command);
+    console.log("response");
 }
