3baf49584b77fd7e765d4d2dd4b6833efe5df024
VIJAYaraaghavan Manoharan <AMVijay@users.noreply.github.com>
2023-12-15 10:18:40 +0530

    lambda function to stop and start ECS tasks

--- ecs/lambda-start-stop-ecs-service/src/index.ts
+++ ecs/lambda-start-stop-ecs-service/src/index.ts
@@ -2,4 +2,11 @@ import { Context, Handler } from "aws-lambda";
 
 export const handler: Handler = async(event: any, context: Context) => {
     console.info("event", JSON.stringify(event));
+    const tasks = fetchTasks(event.clusterName);
+    console.info("Tasks :: ", tasks);
+}
+
+function fetchTasks(){
+
+    
 }
