7dc5acd104b24d906852a576da4ff8e28dab28be
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2024-04-24 06:02:04 +0000

    lambda word to pdf conversion example

--- lambda/work-to-pdf-converstion/src/index.ts
+++ lambda/work-to-pdf-converstion/src/index.ts
@@ -1,4 +1,12 @@
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs'
+
 export const handler = async () => {
     console.log("Hello, World");
-    
+    writeFileSync('/tmp/hello.txt', Buffer.from("Hello, World"));
+    execSync(`
+        cd /tmp
+        libreoffice7.6 --headless --invisible --nodefault --view --nolockcheck --nologo --norestore --convert-to pdf --outdir /tmp ./hello.txt
+    `);
+    console.log("complete PDF Conversion");
 }
